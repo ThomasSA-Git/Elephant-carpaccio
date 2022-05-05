7846ad8e82746b1e0f5a71f882c08dd66d4d5bab
realJimboSlice <jimm7493@stud.kea.dk>
2022-05-05 10:53:55 +0200

    TaxesAndDiscounts

--- src/com/company/TaxesAndDiscounts.java
+++ src/com/company/TaxesAndDiscounts.java
@@ -6,7 +6,7 @@ public class TaxesAndDiscounts {
     double utah = 0.0685;
     double nevada = 0.08;
     double texas = 0.0625;
-    double alberta = 0.04;
+    double alabama = 0.04;
     double california = 0.0825;
   }
 
