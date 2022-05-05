f4a20f3e74cbc78a1e33b0f72e95dcb5e446cf29
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-05-05 10:57:50 +0200

    UI og beregning class tilføjet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,5 @@ public class Main {
         ui.priceOfWares();
         ui.stateTax();
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Input number of wares");
-        int numberOfWares = input.nextInt();
-
-        System.out.println("Input price of wares");
-        int priceWares = input.nextInt();
-
-        System.out.println("Input state tax");
-        String tax = input.next();
     }
 }
