a7158448a9147384fd938b418b595672471d15db
realJimboSlice <jimm7493@stud.kea.dk>
2022-05-05 10:44:11 +0200

    Statetaxes

---
+++ src/com/company/StateTax.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class StateTax {
+
+  public void stateTaxes {
+    double utah = 0.0685;
+    double nevada = 0.08;
+    double texas = 0.0625;
+    double alberta = 0.04;
+    double california = 0.0825;
+  }
+}
