cfcf59a093c44135d0c5c8c35c3a361e5e8ec484
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-05-05 10:41:08 +0200

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input number of wares");
+        int numberOfWares = input.nextInt();
+
+        System.out.println("Input price of wares");
+        int priceWares = input.nextInt();
+
+        System.out.println("Input state tax");
+        String tax = input.next();
     }
 }
